5b6827a7a40eb0af26a06fa3050668269cc50b8c
Sharanya Nemade <87638228+SharanyaNemade@users.noreply.github.com>
2026-02-28 13:42:23 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using JwT_Core_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JwT_Core_MVC.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
